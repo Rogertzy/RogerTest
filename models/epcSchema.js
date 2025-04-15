665440b8a50e2c026b3e1bc50959ffd75d55b2fc
Rogertzy <2334408847@qq.com>
2025-04-16 01:06:52 +0800

    Update epcSchema.js

--- models/epcSchema.js
+++ models/epcSchema.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 // EPC schema definition
 const epcSchema = new mongoose.Schema({
     epc: { type: String, required: true, unique: true }, // EPC must be unique and required
-    title: { type: String, required: true }, // Book title is required
-    author: { type: [String], required: true }, // Author is an array of strings and required
+    title: { type: String, required: true, default: 'Unknown Title' }, // Book title is required
+    author: { type: [String], required: true, default: ['Unknown Author'] }, // Author is an array of strings and required
     status: { 
         type: String, 
         enum: ['borrowed', 'in return box', 'in library'], // Restrict to these values
-        default: 'in return box', // Default status
+        default: 'in library', // Default status
     }, industryIdentifier: {
         type: [String],
     },
