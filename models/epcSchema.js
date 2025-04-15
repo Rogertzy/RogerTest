75aba479227162e8e5b2064bb199d544df0d543f
Rogertzy <2334408847@qq.com>
2025-04-16 00:50:58 +0800

    123

--- models/epcSchema.js
+++ models/epcSchema.js
@@ -4,8 +4,10 @@ const epcSchema = new mongoose.Schema({
   epc: { type: String, required: true, unique: true },
   title: { type: String, default: 'Unknown Title' },
   author: { type: [String], default: ['Unknown Author'] },
-  status: { type: String, enum: ['borrowed', 'in library', 'in return box'], default: 'in library' },
-  readerIp: { type: String, default: null },
+  status: { type: String, 
+    enum: ['borrowed', 'in library', 'in return box'], 
+    default: 'in library' 
+  },
   timestamp: { type: Number, default: Date.now },
   logs: [{
     message: String,
