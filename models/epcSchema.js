6b87b7ca178425b5bb3166b2dbaca5b7c7ab874f
Rogertzy <2334408847@qq.com>
2025-04-16 01:38:30 +0800

    123

--- models/epcSchema.js
+++ models/epcSchema.js
@@ -3,19 +3,16 @@ const mongoose = require('mongoose');
 // EPC schema definition
 const epcSchema = new mongoose.Schema({
     epc: { type: String, required: true, unique: true }, // EPC must be unique and required
-    title: { type: String, default: 'Unknown Title', required: true }, // Book title is required
-    author: { type: [String], default: ['Unknown Author'], required: true }, // Author is an array of strings and required
+    title: { type: String, required: true }, // Book title is required
+    author: { type: [String], required: true }, // Author is an array of strings and required
     status: { 
         type: String, 
         enum: ['borrowed', 'in return box', 'in library'], // Restrict to these values
-        default: 'in library'},
-    readerIp: { type: String, default: null },
-    timestamp: { type: Date, default: Date.now },
-    logs: [{
-      message: String,
-      timestamp: { type: Number, default: Date.now }
-    }], 
-    industryIdentifier: { type: [String]},
+        default: 'in library'
+    }, industryIdentifier: {
+        type: [String],
+    },
+    timestamp: { type: Date, default: Date.now } // Automatically add a timestamp
 });
 
 // Export the EPC model
