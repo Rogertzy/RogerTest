2e564e44750e9ff03f62eaaad22e6aaad9a32313
Rogertzy <2334408847@qq.com>
2025-04-16 01:00:30 +0800

    Update epcSchema.js

--- models/epcSchema.js
+++ models/epcSchema.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 
+// EPC schema definition
 const epcSchema = new mongoose.Schema({
-  epc: { type: String, required: true, unique: true },
-  title: { type: String, default: 'Unknown Title' },
-  author: { type: [String], default: ['Unknown Author'] },
-  status: { type: String, enum: ['borrowed', 'in library', 'in return box'], default: 'in library' },
-  readerIp: { type: String, default: null },
-  timestamp: { type: Number, default: Date.now },
-  logs: [{
-    message: String,
-    timestamp: { type: Number, default: Date.now }
-  }],
-  industryIdentifier: { type: [String], default: ['N/A'] }
+    epc: { type: String, required: true, unique: true }, // EPC must be unique and required
+    title: { type: String, required: true }, // Book title is required
+    author: { type: [String], required: true }, // Author is an array of strings and required
+    status: { 
+        type: String, 
+        enum: ['borrowed', 'in return box', 'in library'], // Restrict to these values
+        default: 'in return box', // Default status
+    }, industryIdentifier: {
+        type: [String],
+    },
+    timestamp: { type: Date, default: Date.now } // Automatically add a timestamp
 });
 
-module.exports = mongoose.model('Epc', epcSchema);
+// Export the EPC model
+module.exports = mongoose.model('EPC', epcSchema);
